5a28ee174bbdc114f7b8ac9e39410ad4ba76e2dd
Aaron Park <aaronj.park0304@gmail.com>
2024-07-08 02:24:09 -0500

    changed html files to ejs files

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+// static files
+app.use(express.static("public"));
+
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/expertise", (req, res) => {
+    res.render("expertise.ejs");
+})
+
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs");
+})
+
+app.get("/experiences", (req, res) => {
+    res.render("experiences.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+})
