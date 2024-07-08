fc38938a9b257216f2f89b5a24621ceed1d3bf9c
Aaron Park <aaronj.park0304@gmail.com>
2024-07-08 03:51:53 -0500

    nav bar animation not working

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = 3000;
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
 
 // static files
 app.use(express.static("public"));
 
-// middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
+// routes
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
